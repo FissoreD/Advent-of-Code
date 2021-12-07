8996763452ab6bc75eb59b2f44be02f9587ec2c3
FissoreD <79260701+FissoreD@users.noreply.github.com>
2021-12-07 18:03:41 +0100

    day7

--- 2021/src/day7.py
+++ 2021/src/day7.py
@@ -1,1 +1,35 @@
+from typing import Counter
 f = './inp/day7.txt'
+
+
+def get_line():
+    with open(f) as F:
+        return Counter(map(int, F.readline().strip().split(',')))
+
+
+def day7_1():
+    C = get_line()
+    current_value = float('inf')
+    for c in C:
+        c1 = sum([abs(x - c)*C[x] for x in C])
+        if c1 < current_value:
+            current_value = c1
+    return current_value
+
+
+def day7_2():
+    C = get_line()
+    current_value = float('inf')
+    for c in range(max(C)):
+        def val(x): return abs(x - c)
+        def sumX(x): return val(x)*(val(x)+1)//2
+        c1 = sum([sumX(x)*C[x] for x in C])
+        if c1 < current_value:
+            current_value = c1
+        if c1 > current_value:
+            break
+    return current_value
+
+
+print(day7_1())
+print(day7_2())
